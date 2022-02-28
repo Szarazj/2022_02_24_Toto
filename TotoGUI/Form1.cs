fb7fb31566a1de625e1272da61bfe7f9ac31760d
szrz_jzsf44 <szarazj.12d@acsjszki.hu>
2022-02-28 13:19:25 +0100

    12c

--- TotoGUI/Form1.cs
+++ TotoGUI/Form1.cs
@@ -17,6 +17,7 @@ namespace TotoGUI
             InitializeComponent();
         }     
 
+        //10.b
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             if (textBox1.Text.Length == 14)
@@ -28,6 +29,20 @@ namespace TotoGUI
                 checkBox1.Checked = true;
             }
             checkBox1.Text = $"Nem megfelelő a karakterek száma ({textBox1.Text.Length})";
+
+            //10.c
+            string hibásKarakterek = "";
+            foreach (var ch in textBox1.Text)
+            {
+                if (ch != '1' && ch != '2' && ch != 'X')
+                {
+                    hibásKarakterek += ch + ";";
+                }
+                checkBox2.Text = $"Helytelen karakterek az eredményekben ({hibásKarakterek})";
+
+
+            }
+
         }
 
     }
